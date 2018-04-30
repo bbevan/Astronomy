f99797524d151da7a468bc78282f2e51a1d5b9b1
bbevan <nb00215@georgiasouthern.edu>
2018-04-30 16:48:19 -0400

    functioned

--- fitsreport.py
+++ fitsreport.py
@@ -4,17 +4,21 @@ import fnmatch
 
 a = os.walk('/home/brandon/Documents/a')
 
-for root, dir, files in a:
-    for items in files:
+def report():
+    for root, dir, files in a:
+        for items in files:
             #for name in items:
             #fits_image_filename = fits.util.get_testdata_filepath(name)
-            if fnmatch.fnmatch(items, "*.fits"):
-                name = os.path.join(root, items)
-                print(name)
-                hdul = fits.open(name)
-                print("Date:\t\t" + hdul[0].header['DATE'])
-                print("Filter:\t\t" + hdul[0].header['FILTER2'])
-                print("Object:\t\t" + hdul[0].header['OBJECT'])
-                print("Binning:\t" + hdul[1].header['CCDSUM'])
-                print("\n\n")
-                hdul.close()
+                if fnmatch.fnmatch(items, "*.fits"):
+                    name = os.path.join(root, items)
+                    print(name)
+                    hdul = fits.open(name)
+                    print("Date:\t\t" + hdul[0].header['DATE'])
+                    print("Filter:\t\t" + hdul[0].header['FILTER2'])
+                    print("Object:\t\t" + hdul[0].header['OBJECT'])
+                    print("Binning:\t" + hdul[1].header['CCDSUM'])
+                    print("\n\n")
+                    hdul.close()
+
+#run report
+report()
